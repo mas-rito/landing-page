02cf4cf9186345b3c51fc1d254b6fb7e53ca1312
Rito Ramadhan <ritoramadhan8@gmail.comgit config --global user.name Rito>
2023-10-27 00:47:27 +0700

    update: make layout hero section responsive

--- src/components/Fragments/Navbar.jsx
+++ src/components/Fragments/Navbar.jsx
@@ -5,10 +5,12 @@ const Navbar = () => {
 
   return (
     <nav className="fixed top-0 left-0 w-full text-white flex justify-between items-center backdrop-blur-sm px-8 py-4">
-      <h1 className="text-3xl font-bold">HERNY JUNIARTI, S.E</h1>
+      <h1 className="text-xl md:text-2xl lg:text-3xl font-bold">
+        HERNY JUNIARTI, S.E
+      </h1>
       <button
         onClick={() => setOpen(!open)}
-        className="px-3 py-2 border rounded text-white border-white hover:text-blue-500 hover:border-blue-500 absolute right-8 cursor-pointer md:hidden"
+        className="px-3 py-2 border rounded text-white border-white hover:text-blue-500 hover:border-blue-500 absolute right-8 cursor-pointer lg:hidden"
       >
         <svg
           className=" fill-current h-3 w-3"
@@ -20,30 +22,30 @@ const Navbar = () => {
         </svg>
       </button>
       <ul
-        className={`absolute right-0 md:static md:flex gap-6 transition-all duration-300 ease-in bg-slate-300 md:bg-transparent rounded-lg text-black md:py-0 py-5 md:text-white ${
+        className={`absolute right-0 lg:static lg:flex gap-6 transition-all duration-300 ease-in bg-slate-300 lg:bg-transparent rounded-lg text-black lg:py-0 py-5 lg:text-white ${
           open ? "top-16" : "top-[-200px]"
         }`}
       >
-        <li className="hover:text-blue-500 hover:border-blue-500 px-20 py-1 md:p-0">
+        <li className="hover:text-blue-500 hover:border-blue-500 px-20 py-1 lg:p-0">
           Home
         </li>
-        <li className="hover:text-blue-500 hover:border-blue-500 px-20 py-1 md:p-0">
+        <li className="hover:text-blue-500 hover:border-blue-500 px-20 py-1 lg:p-0">
           Biodata Saya
         </li>
-        <li className="hover:text-blue-500 hover:border-blue-500 px-20 py-1 md:p-0">
+        <li className="hover:text-blue-500 hover:border-blue-500 px-20 py-1 lg:p-0">
           Pengalaman
         </li>
-        <li className="hover:text-blue-500 hover:border-blue-500 px-20 py-1 md:p-0">
+        <li className="hover:text-blue-500 hover:border-blue-500 px-20 py-1 lg:p-0">
           Organisasi
         </li>
-        <li className="hover:text-blue-500 hover:border-blue-500 px-20 py-1 md:p-0">
+        <li className="hover:text-blue-500 hover:border-blue-500 px-20 py-1 lg:p-0">
           Dokumentasi
         </li>
       </ul>
       <img
         src={logoPan}
         alt="PAN bantu rakyat"
-        className="h-10 me-4 hidden md:block"
+        className="h-10 me-4 hidden lg:block"
       />
     </nav>
   );
--- src/components/Layouts/HeroSection.jsx
+++ src/components/Layouts/HeroSection.jsx
@@ -9,15 +9,19 @@ const HeroSection = () => {
         style={{ backgroundImage: `url(${backgroundHero})` }}
       >
         <Container>
-          <div className="flex">
-            <div className="flex flex-col w-2/3">
-              <div className="flex justify-between text-white text-4xl font-bold my-4">
+          <div className="flex flex-wrap md:flex-nowrap">
+            <div className="flex flex-col w-full md:w-2/3">
+              <div className="flex flex-wrap md:flex-nowrap justify-center md:justify-between text-white text-center text-2xl md:text-3xl lg:text-4xl font-bold my-4">
                 <h1>#WanitaPermataMulia</h1>
                 <h1>#HernyTerbuktiAmanah</h1>
               </div>
-              <img src={fotoGroup} className="" alt="" />
+              <img src={fotoGroup} className="hidden md:block" alt="" />
             </div>
-            <img src={buHerny} alt="Herny Junarti, S.E" className="w-1/3" />
+            <img
+              src={buHerny}
+              alt="Herny Junarti, S.E"
+              className="w-full md:w-1/3"
+            />
           </div>
         </Container>
       </div>
