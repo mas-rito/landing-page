33acc1a2280a18d1de7b3c6fd7adaa881b8e27ba
Rito Ramadhan <126365429+mas-rito@users.noreply.github.com>
2024-01-17 10:06:32 +0700

    update(Navbar and asset): change struktur navbar and asset

---
+++ public/assets/index.js
@@ -0,0 +1,67 @@
+import logoPan from "./image/logo-pan.png";
+import backgroundHero from "./image/background.png";
+import fotoGroup from "./image/foto-group-hero.png";
+import buHerny from "./image/foto-bu-herny.png";
+import buHernySalam from "./image/bu-herny-salam.png";
+
+import bgCardTop from "./elements/bg-card-top.png";
+import bgCardBottom from "./elements/bg-card-bottom.png";
+import titleIcon from "./elements/title-icon.svg";
+import organisasiBg from "./elements/organisasi-bg.svg";
+import plus from "./elements/plus.svg";
+import backgroundFooter from "./elements/footer-bg.svg";
+
+import bankBali from "./logo/bank-bali.png";
+import hanwa from "./logo/hanwa.png";
+import samsung from "./logo/samsung.png";
+import sunlife from "./logo/sunlife.png";
+import telkomsel from "./logo/telkomsel.png";
+import building from "./logo/building.svg";
+
+import galeri1 from "./image/galeri1.png";
+import galeri2 from "./image/galeri2.png";
+import galeri3 from "./image/galeri3.png";
+import galeri4 from "./image/galeri4.png";
+import galeri5 from "./image/galeri5.png";
+import galeri6 from "./image/galeri6.png";
+import galeri7 from "./image/galeri7.png";
+import galeri8 from "./image/galeri8.png";
+import galeri9 from "./image/galeri9.png";
+
+import twitter from "./logo/twitter.svg";
+import facebook from "./logo/facebook.svg";
+import instagram from "./logo/instagram.svg";
+import tiktok from "./logo/tiktok.svg";
+
+export {
+  logoPan,
+  backgroundHero,
+  fotoGroup,
+  buHerny,
+  buHernySalam,
+  bgCardTop,
+  bgCardBottom,
+  titleIcon,
+  organisasiBg,
+  plus,
+  backgroundFooter,
+  bankBali,
+  hanwa,
+  samsung,
+  sunlife,
+  telkomsel,
+  building,
+  galeri1,
+  galeri2,
+  galeri3,
+  galeri4,
+  galeri5,
+  galeri6,
+  galeri7,
+  galeri8,
+  galeri9,
+  twitter,
+  facebook,
+  instagram,
+  tiktok,
+};
--- src/components/Fragments/Navbar.jsx
+++ src/components/Fragments/Navbar.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { logoPan } from "../../assets";
 const Navbar = () => {
   const [open, setOpen] = useState(false);
   const [isScroll, setIsScroll] = useState(false);
@@ -18,54 +17,76 @@ const Navbar = () => {
   }, []);
 
   return (
-    <nav
-      className={`fixed top-0 left-0 w-full duration-500 ${
-        scrollY ? "text-blue-800" : "text-white"
-      } flex justify-between items-center backdrop-blur-sm ${
-        isScroll ? "bg-blue-50 shadow-md" : "bg-transparent shadow-none"
-      }  px-8 py-4 z-40`}
-    >
-      <h1 className="text-xl md:text-2xl lg:text-3xl font-bold">
-        HERNY JUNIARTI, S.E
-      </h1>
-      <button
-        onClick={() => setOpen(!open)}
-        className={`px-3 py-2 border rounded ${
-          scrollY ? "text-blue-800 border-blue-800" : "text-white border-white"
-        }  hover:text-blue-500 hover:border-blue-500 absolute right-8 cursor-pointer lg:hidden`}
-      >
-        <svg
-          className=" fill-current h-3 w-3"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
+    <nav className="fixed top-0 left-0 z-40 w-full">
+      <div className="relative">
+        <div
+          className={`flex justify-between items-center duration-500 lg:backdrop-blur-sm ${
+            isScroll
+              ? "bg-white shadow-md text-blue-800"
+              : "bg-transparent shadow-none text-white"
+          }  px-8 py-4`}
         >
-          <title>Menu</title>
-          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-        </svg>
-      </button>
-      <ul
-        className={`absolute right-0 lg:static lg:flex gap-6 transition-all duration-300 ease-in bg-blue-50 lg:bg-transparent rounded-lg lg:py-0 py-5 ${
-          scrollY ? "text-blue-800" : "text-black lg:text-white"
-        }  ${open ? "top-16" : "top-[-200px]"}`}
-      >
-        <li className="hover:text-blue-500 hover:border-blue-500 px-20 py-1 lg:p-0">
-          <a href="#home">Home</a>
-        </li>
-        <li className="hover:text-blue-500 hover:border-blue-500 px-20 py-1 lg:p-0">
-          <a href="#about">Biodata Saya</a>
-        </li>
-        <li className="hover:text-blue-500 hover:border-blue-500 px-20 py-1 lg:p-0">
-          <a href="#experience">Pengalaman</a>
-        </li>
-        <li className="hover:text-blue-500 hover:border-blue-500 px-20 py-1 lg:p-0">
-          <a href="#docimentation">Dokumentasi</a>
-        </li>
-      </ul>
-      <img
-        src={logoPan}
-        alt="PAN bantu rakyat"
-        className="h-10 me-4 hidden lg:block"
-      />
+          <h1 className="text-xl md:text-2xl lg:text-3xl font-bold">
+            HERNY JUNIARTI, S.E
+          </h1>
+          <button
+            onClick={() => setOpen(!open)}
+            className={`px-3 py-2 border rounded ${
+              isScroll
+                ? "text-blue-800 border-blue-800"
+                : "text-white border-white"
+            }  hover:text-blue-500 hover:border-blue-500 absolute right-8 cursor-pointer lg:hidden`}
+          >
+            <svg
+              className=" fill-current h-3 w-3"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
+          <ul className="hidden md:flex gap-6">
+            <li className="hover:text-blue-500 hover:border-blue-500 px-20 py-1 lg:p-0">
+              <a href="#home">Home</a>
+            </li>
+            <li className="hover:text-blue-500 hover:border-blue-500 px-20 py-1 lg:p-0">
+              <a href="#about">Biodata Saya</a>
+            </li>
+            <li className="hover:text-blue-500 hover:border-blue-500 px-20 py-1 lg:p-0">
+              <a href="#experience">Pengalaman</a>
+            </li>
+            <li className="hover:text-blue-500 hover:border-blue-500 px-20 py-1 lg:p-0">
+              <a href="#docimentation">Dokumentasi</a>
+            </li>
+          </ul>
+          <img
+            src="/assets/image/logo-pan.png"
+            alt="PAN bantu rakyat"
+            className="h-10 me-4 hidden lg:block"
+          />
+        </div>
+        <div
+          className={`absolute ${
+            open ? "" : "translate-y-[-150%]"
+          } left-0 w-full lg:hidden bg-white shadow-md -z-10 transition-transform duration-300`}
+        >
+          <ul className="flex flex-col items-center gap-6 py-4 font-bold">
+            <li className="hover:text-blue-500 hover:border-blue-500 px-20 py-1 lg:p-0">
+              <a href="#home">Home</a>
+            </li>
+            <li className="hover:text-blue-500 hover:border-blue-500 px-20 py-1 lg:p-0">
+              <a href="#about">Biodata Saya</a>
+            </li>
+            <li className="hover:text-blue-500 hover:border-blue-500 px-20 py-1 lg:p-0">
+              <a href="#experience">Pengalaman</a>
+            </li>
+            <li className="hover:text-blue-500 hover:border-blue-500 px-20 py-1 lg:p-0">
+              <a href="#docimentation">Dokumentasi</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
